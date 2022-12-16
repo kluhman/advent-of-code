7ee4c52aba0593c1a2bb84b6504dfaf2954f6013
Kaleb Luhman <kaleb_luhman@carmax.com>
2022-12-15 18:48:40 -0600

    optimizes beacon finding problem using premise that the distress signal is located in the gap between 2 sensors, and finding the intersection of 2 sensors boundaries

--- src/AdventOfCode2022/Day15BeaconExclusionZone.cs
+++ src/AdventOfCode2022/Day15BeaconExclusionZone.cs
@@ -16,34 +16,29 @@ public class Day15BeaconExclusionZone : IChallenge
         return combinedRanges.Sum(range => range.End - range.Start);
     }
 
+    /// <summary>
+    ///     This solutions works on the basis that there will only ever be one remaining space in the allowed range that the
+    ///     beacon will be.
+    ///     The point the beacon is located at will be (sensorDistance + 1) away from at least 2 sensors, which means if we
+    ///     draw lines representing
+    ///     the outer boundary of each sensor, the intersections of those boundaries will give us all the possible locations
+    ///     where 2 sensors boundaries meet.
+    ///     Then if we filter to those within the allowed range, only one should remain.
+    /// </summary>
     public object SolvePart2(string input)
     {
         var maxRange = new Range<int>(0, 4000000);
         var sensors = ParseSensors(input).ToImmutableArray();
+        var boundaryLines = sensors.SelectMany(sensor => sensor.GetBoundaryLines()).ToList();
 
-        var tuningFrequency = 0L;
-        Parallel.For(maxRange.Start, maxRange.End + 1, y =>
-        {
-            var sensorXRanges = GetCombinedRangesOnY(sensors, y, maxRange).ToList();
-            if (sensorXRanges.Count < 1)
-            {
-                return;
-            }
+        var boundaryIntersections = boundaryLines
+            .SelectMany(line1 => boundaryLines.Select(line1.GetIntersection))
+            .Where(point => point is not null && maxRange.Contains(point.X) && maxRange.Contains(point.Y))
+            .Distinct();
 
-            var firstRange = sensorXRanges.First();
-            if (firstRange == maxRange)
-            {
-                return;
-            }
+        var distressBeaconLocation = boundaryIntersections.Single(point => !sensors.Any(sensor => sensor.Covers(point!)))!;
 
-            var x = (long)(firstRange.Start == maxRange.Start
-                ? firstRange.End + 1
-                : firstRange.Start - 1);
-
-            tuningFrequency = x * 4000000 + y;
-        });
-
-        return tuningFrequency;
+        return distressBeaconLocation.X * 4000000L + distressBeaconLocation.Y;
     }
 
     private static IEnumerable<Sensor> ParseSensors(string input) => input
@@ -94,27 +89,49 @@ public class Day15BeaconExclusionZone : IChallenge
         }
     }
 
-    private record Point(int X, int Y);
+    private record Point(int X, int Y)
+    {
+        public int DistanceTo(Point other)
+        {
+            var xSteps = Math.Abs(X - other.X);
+            var ySteps = Math.Abs(Y - other.Y);
+            return xSteps + ySteps;
+        }
+    }
+
+    private record Line(Point Start, Point End)
+    {
+        public Point? GetIntersection(Line other)
+        {
+            // this solution is based on math found here:
+            // https://github.com/chrisleow/advent-of-code/blob/main/2022/src/Day15.kt
+            var c1 = CalculateC(this) ?? CalculateC(other);
+            if (c1 is null)
+            {
+                return null;
+            }
+
+            var c2 = CalculateC(this, -1) ?? CalculateC(other, -1);
+            if (c2 is null)
+            {
+                return null;
+            }
+
+            return new Point((c1.Value + c2.Value) / 2, (c1.Value - c2.Value) / 2);
+        }
+
+        private static int? CalculateC(Line line, int multiplier = 1)
+        {
+            var c1 = line.Start.X + line.Start.Y * multiplier;
+            var c2 = line.End.X + line.End.Y * multiplier;
+
+            return c1 == c2 ? c1 : null;
+        }
+    }
 
     private record Sensor(Point Location, Point ClosestBeacon)
     {
-        public int Distance { get; } = CalculateDistance(Location, ClosestBeacon);
-
-        public static Sensor Parse(string line)
-        {
-            var match = Regex.Match(line, @"Sensor at x=(?<sensorX>-?\d+), y=(?<sensorY>-?\d+): closest beacon is at x=(?<beaconX>-?\d+), y=(?<beaconY>-?\d+)");
-            var sensorLocation = new Point(int.Parse(match.Groups["sensorX"].Value), int.Parse(match.Groups["sensorY"].Value));
-            var beaconLocation = new Point(int.Parse(match.Groups["beaconX"].Value), int.Parse(match.Groups["beaconY"].Value));
-
-            return new Sensor(sensorLocation, beaconLocation);
-        }
-
-        private static int CalculateDistance(Point point1, Point point2)
-        {
-            var xSteps = Math.Abs(point1.X - point2.X);
-            var ySteps = Math.Abs(point1.Y - point2.Y);
-            return xSteps + ySteps;
-        }
+        public int Distance { get; } = Location.DistanceTo(ClosestBeacon);
 
         public Range<int>? GetRangeOnY(int y)
         {
@@ -128,6 +145,31 @@ public class Day15BeaconExclusionZone : IChallenge
             var ySteps = Math.Abs(Location.Y - y);
             var xSteps = Distance - ySteps;
             return new Range<int>(Location.X - xSteps, Location.X + xSteps);
+        }
+
+        public IEnumerable<Line> GetBoundaryLines()
+        {
+            var boundaryDistance = Distance + 1;
+            var left = Location with { X = Location.X - boundaryDistance };
+            var right = Location with { X = Location.X + boundaryDistance };
+            var down = Location with { Y = Location.Y - boundaryDistance };
+            var up = Location with { Y = Location.Y + boundaryDistance };
+
+            yield return new Line(left, up);
+            yield return new Line(left, down);
+            yield return new Line(right, up);
+            yield return new Line(right, down);
+        }
+
+        public bool Covers(Point point) => Location.DistanceTo(point) <= Distance;
+
+        public static Sensor Parse(string line)
+        {
+            var match = Regex.Match(line, @"Sensor at x=(?<sensorX>-?\d+), y=(?<sensorY>-?\d+): closest beacon is at x=(?<beaconX>-?\d+), y=(?<beaconY>-?\d+)");
+            var sensorLocation = new Point(int.Parse(match.Groups["sensorX"].Value), int.Parse(match.Groups["sensorY"].Value));
+            var beaconLocation = new Point(int.Parse(match.Groups["beaconX"].Value), int.Parse(match.Groups["beaconY"].Value));
+
+            return new Sensor(sensorLocation, beaconLocation);
         }
     }
 }
