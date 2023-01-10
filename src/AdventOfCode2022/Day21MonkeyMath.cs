b3b14686daedda30266543c3afc60349aeb077f3
Kaleb Luhman <kaleb_luhman@carmax.com>
2023-01-10 14:22:08 -0600

    solves part 2 of monkey math - solving for variable number, improves performance by using dictionary for monkey lookups instead of list search

--- src/AdventOfCode2022/Day21MonkeyMath.cs
+++ src/AdventOfCode2022/Day21MonkeyMath.cs
@@ -5,32 +5,94 @@ namespace AdventOfCode2022;
 
 public class Day21MonkeyMath : IChallenge
 {
-    public enum Operator
-    {
-        Add,
-        Multiply,
-        Subtract,
-        Divide
-    }
-
     public int ChallengeId => 21;
 
     public object SolvePart1(string input)
     {
-        var monkeys = ParseMonkeys(input).ToList();
-        var root = monkeys.Single(x => x.Id == "root");
-
-        root.Expression.TryEvaluate(monkeys, out var result);
+        var monkeys = ParseMonkeys(input);
+        monkeys["root"].Expression.TryEvaluate(monkeys, out var result);
         return result;
     }
 
-    public object SolvePart2(string input) => 0;
+    public object SolvePart2(string input)
+    {
+        var monkeys = ParseMonkeys(input);
+        var root = MakeRootEqualityCheck(monkeys);
+        MakeHumanVariable(monkeys);
 
-    private static IEnumerable<Monkey> ParseMonkeys(string input) => input
+        return SolveForVariable(0, root.Expression, monkeys);
+    }
+
+    private static Dictionary<string, Monkey> ParseMonkeys(string input) => input
         .Split('\n', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
-        .Select(Monkey.Parse);
+        .Select(Monkey.Parse)
+        .ToDictionary(x => x.Id, x => x);
 
-    public class Monkey
+    private static Monkey MakeRootEqualityCheck(IDictionary<string, Monkey> monkeys)
+    {
+        var root = monkeys["root"];
+        var originalExpression = (OperationExpression)root.Expression;
+
+        root = new Monkey(root.Id, new OperationExpression(originalExpression.Left, Operator.CheckEquality, originalExpression.Right));
+        monkeys[root.Id] = root;
+
+        return root;
+    }
+
+    private static void MakeHumanVariable(IDictionary<string, Monkey> monkeys)
+    {
+        var human = monkeys["humn"];
+        human = new Monkey(human.Id, new VariableExpression());
+        monkeys[human.Id] = human;
+    }
+
+    private static long SolveForVariable(long valueNeeded, Expression equation, IReadOnlyDictionary<string, Monkey> monkeys)
+    {
+        if (equation is not OperationExpression operation)
+        {
+            return valueNeeded;
+        }
+
+        // check if we know the left side of the equation
+        if (operation.Left.TryEvaluate(monkeys, out var left))
+        {
+            valueNeeded = ReverseOperation(valueNeeded, left, true, operation.Op);
+            return SolveForVariable(valueNeeded, operation.Right.Reduce(monkeys), monkeys);
+        }
+
+        // if we don't know the left, then we know the right since there is only one variable in this problem
+        operation.Right.TryEvaluate(monkeys, out var right);
+
+        valueNeeded = ReverseOperation(valueNeeded, right, false, operation.Op);
+        return SolveForVariable(valueNeeded, operation.Left.Reduce(monkeys), monkeys);
+    }
+
+    private static long ReverseOperation(long valueNeeded, long knownValue, bool isLeft, Operator op) => op switch
+    {
+        Operator.CheckEquality => knownValue,
+        Operator.Add => valueNeeded - knownValue,
+        Operator.Multiply => valueNeeded / knownValue,
+
+        // need to subtract or add based on whether we know the left or right value
+        Operator.Subtract when isLeft => knownValue - valueNeeded,
+        Operator.Subtract => valueNeeded + knownValue,
+
+        // need to divide or add based on whether we know the left or right value
+        Operator.Divide when isLeft => knownValue / valueNeeded,
+        Operator.Divide => valueNeeded * knownValue,
+        _ => throw new ArgumentOutOfRangeException(nameof(op), op, null)
+    };
+
+    private enum Operator
+    {
+        Add,
+        Multiply,
+        Subtract,
+        Divide,
+        CheckEquality
+    }
+
+    private class Monkey
     {
         public Monkey(string id, Expression expression)
         {
@@ -48,9 +110,11 @@ public class Day21MonkeyMath : IChallenge
         }
     }
 
-    public abstract class Expression
+    private abstract class Expression
     {
-        public abstract bool TryEvaluate(IReadOnlyCollection<Monkey> monkeys, out long result);
+        public abstract bool TryEvaluate(IReadOnlyDictionary<string, Monkey> monkeys, out long result);
+
+        public virtual Expression Reduce(IReadOnlyDictionary<string, Monkey> monkeys) => this;
 
         public static Expression Parse(string input)
         {
@@ -76,7 +140,7 @@ public class Day21MonkeyMath : IChallenge
         }
     }
 
-    public class NumberExpression : Expression
+    private class NumberExpression : Expression
     {
         public NumberExpression(long number)
         {
@@ -85,14 +149,14 @@ public class Day21MonkeyMath : IChallenge
 
         public long Number { get; }
 
-        public override bool TryEvaluate(IReadOnlyCollection<Monkey> monkeys, out long result)
+        public override bool TryEvaluate(IReadOnlyDictionary<string, Monkey> monkeys, out long result)
         {
             result = Number;
             return true;
         }
     }
 
-    public class OperationExpression : Expression
+    private class OperationExpression : Expression
     {
         public OperationExpression(Expression left, Operator op, Expression right)
         {
@@ -105,7 +169,7 @@ public class Day21MonkeyMath : IChallenge
         public Operator Op { get; }
         public Expression Right { get; }
 
-        public override bool TryEvaluate(IReadOnlyCollection<Monkey> monkeys, out long result)
+        public override bool TryEvaluate(IReadOnlyDictionary<string, Monkey> monkeys, out long result)
         {
             result = 0;
             if (!Left.TryEvaluate(monkeys, out var left) || !Right.TryEvaluate(monkeys, out var right))
@@ -126,7 +190,7 @@ public class Day21MonkeyMath : IChallenge
         }
     }
 
-    public class MonkeyExpression : Expression
+    private class MonkeyExpression : Expression
     {
         public MonkeyExpression(string monkeyId)
         {
@@ -135,16 +199,16 @@ public class Day21MonkeyMath : IChallenge
 
         public string MonkeyId { get; }
 
-        public override bool TryEvaluate(IReadOnlyCollection<Monkey> monkeys, out long result)
-        {
-            var monkey = monkeys.Single(x => x.Id == MonkeyId);
-            return monkey.Expression.TryEvaluate(monkeys, out result);
-        }
+        public override bool TryEvaluate(IReadOnlyDictionary<string, Monkey> monkeys, out long result) =>
+            Reduce(monkeys).TryEvaluate(monkeys, out result);
+
+        public override Expression Reduce(IReadOnlyDictionary<string, Monkey> monkeys) =>
+            monkeys[MonkeyId].Expression;
     }
 
-    public class VariableExpression : Expression
+    private class VariableExpression : Expression
     {
-        public override bool TryEvaluate(IReadOnlyCollection<Monkey> monkeys, out long result)
+        public override bool TryEvaluate(IReadOnlyDictionary<string, Monkey> monkeys, out long result)
         {
             result = 0;
             return false;
